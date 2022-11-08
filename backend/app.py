917e550a5d82c6cae7c3214a85875241891f7704
Stuflo19 <szflorence@dundee.ac.uk>
2022-11-08 13:45:33 +0000

    Updated docker compose

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {'time': time.time(), 'your mum': "fat"}
 
 if __name__ == "__main__": app.run()
